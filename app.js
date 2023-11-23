7eaa16acea1ef5e5665603229ab70444d7153de3
Gustavo Colombelli <gustavocolombelli@gmail.com>
2023-11-23 20:49:43 -0300

    cords -> cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { livros } = require('./livros');
-const cors = require('cords')
+const cors = require('cors')
 const app = express();
 const port = 3000;
 
