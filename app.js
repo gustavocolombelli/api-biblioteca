b92079f158c7e5b67508638a3eaf4695a6aecb5b
Gustavo Colombelli <gustavocolombelli@gmail.com>
2023-11-23 18:17:20 -0300

    a

--- app.js
+++ app.js
@@ -1,46 +1,7 @@
 const express = require('express');
+const { livros } = require('./livros');
 const app = express();
 const port = 3000;
-
-// Simulando uma lista de livros
-const livros = [
-  {
-    codigo: "LC123",
-    titulo: "Dom Casmurro",
-    autor: "Machado de Assis",
-    anoPublicacao: 1899,
-    emprestado: false,
-    usuarioEmprestimo: null,
-    genero: "Drama"
-  },
-  {
-    codigo: "LT456",
-    titulo: "It",
-    autor: "Stephen King",
-    anoPublicacao: 1986,
-    emprestado: false,
-    usuarioEmprestimo: null,
-    genero: "Terror"
-  },
-  {
-    codigo: "RN789",
-    titulo: "National Geographic",
-    autor: "Vários Autores",
-    anoPublicacao: 2022,
-    emprestado: false,
-    usuarioEmprestimo: null,
-    edicao: "Março"
-  },
-  {
-    codigo: "RN789",
-    titulo: "National Geographic",
-    autor: "Vários Autores",
-    anoPublicacao: 2022,
-    emprestado: false,
-    usuarioEmprestimo: null,
-    edicao: "Março"
-  }
-];
 
 // Rota para obter a lista de livros
 app.get('/acervo', (req, res) => {
---
+++ livros.js
@@ -0,0 +1,40 @@
+// Simulando uma lista de livros
+const livros = [
+  {
+    codigo: "LC123",
+    titulo: "Dom Casmurro",
+    autor: "Machado de Assis",
+    anoPublicacao: 1899,
+    emprestado: false,
+    usuarioEmprestimo: null,
+    genero: "Drama"
+  },
+  {
+    codigo: "LT456",
+    titulo: "It",
+    autor: "Stephen King",
+    anoPublicacao: 1986,
+    emprestado: false,
+    usuarioEmprestimo: null,
+    genero: "Terror"
+  },
+  {
+    codigo: "RN789",
+    titulo: "National Geographic",
+    autor: "Vários Autores",
+    anoPublicacao: 2022,
+    emprestado: false,
+    usuarioEmprestimo: null,
+    edicao: "Março"
+  },
+  {
+    codigo: "RN789",
+    titulo: "National Geographic",
+    autor: "Vários Autores",
+    anoPublicacao: 2022,
+    emprestado: false,
+    usuarioEmprestimo: null,
+    edicao: "Março"
+  }
+];
+exports.livros = livros;
