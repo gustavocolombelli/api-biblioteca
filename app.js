a5ca87b62a33e65820e0717d87d29af53bcd1371
Gustavo Colombelli <gustavocolombelli@gmail.com>
2023-11-23 20:59:33 -0300

    users

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const express = require('express');
 const { livros } = require('./livros');
+const { users } = require('./users');
+
+const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = 3000;
@@ -9,6 +11,10 @@ app.use(cors());
 
 app.get('/acervo', (req, res) => {
   res.json(livros);
+});
+
+app.get('/users', (req, res)=>{
+  res.json(users);
 });
 
 app.listen(port, '0.0.0.0', () => {
---
+++ users.js
@@ -0,0 +1,38 @@
+
+const users = [
+    {
+        nome: "Pedro",
+        registroAcademico: "552031",
+        dataNascimento: "1993-01-01"
+    },
+    {
+        nome: "João",
+        registroAcademico: "11025",
+        dataNascimento: "1993-12-29"
+    },
+    {
+        nome: "Augusto",
+        registroAcademico: "22305",
+        dataNascimento: "1990-08-21"
+    },
+    {
+        nome: "Maria",
+        registroAcademico: "77145",
+        dataNascimento: "1993-03-21"
+    },
+    {
+        nome: "Regina",
+        registroAcademico: "66588",
+        dataNascimento: "1993-07-17"
+    },
+    {
+        nome: "Matilde",
+        registroAcademico: "32225",
+        dataNascimento: "1993-12-11"
+    },
+    
+]
+
+
+
+exports.users = users;
